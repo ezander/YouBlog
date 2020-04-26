be281f57711f96e101a543c68601e4bd48e4cbc5
Elmar Zander <e.zander@tu-bs.de>
2020-04-26 10:13:57 +0200

    FillDatabase writes to Firestore now

--- data/FillDatabase.sh
+++ data/FillDatabase.sh
@@ -9,18 +9,49 @@ export DATABASEURL="https://youblog-814ae.firebaseio.com"
 export PROJECT_ID="youblog-814ae"
 
 
+function write_post_realtime() {
+    content=$(cat $5 | sed 's/"/\\\"/g' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
+    url="https://${PROJECT_ID}.firebaseio.com/blog_entries.json"
+    json=$(cat <<EOF
+{
+    "author" : "$1", 
+    "author_id" : "$2", 
+    "title" : "$4", 
+    "date" : "$3", 
+    "text" : "$content",
+    "image_url" : "$6"
+}
+EOF
+)
+    curl -vvv -X POST -d "$json" "$url"
+    
+}
+
+function write_post_firestore() {
+    content=$(cat $5 | sed 's/"/\\\"/g' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
+    collection="blog_entries"
+    name="projects/${PROJECT_ID}/databases/(default)/documents/${collection}?documentId=$5"
+    url="https://firestore.googleapis.com/v1/$name"
+    json=$(cat <<EOF
+{
+    "fields": {
+        "author" : {"stringValue": "$1"},
+        "author_id" : {"stringValue": "$2"},
+        "title" : {"stringValue": "$4"},
+        "date" : {"timestampValue": "$3T00:00:00Z"},
+        "text" : {"stringValue": "$content"},
+        "image_url" : {"stringValue": "$6"}
+        }
+}
+EOF
+)
+    # echo curl -X POST -d "$json" "$url"
+    curl -X POST -H "Content-Type: application/jsons" -d "$json" "$url"
+}
+
 function write_post() {
-content=$(cat $5 | sed 's/"/\\\"/g' | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
-curl -X POST -d "{
-    \"author\" : \"$1\", 
-    \"author_id\" : \"$2\", 
-    \"title\" : \"$4\", 
-    \"date\" : \"$3\", 
-    \"text\" : \"$content\",
-    \"image_url\" : \"$6\"
-    }" \
-    "https://${PROJECT_ID}.firebaseio.com/blog_entries.json"
-echo ""
+    write_post_firestore "$@"
+    # write_post_realtime "$@"
 }
 
 write_post "Chris Coyier" "_cc" "2016-04-27" "You Should Probably Blog in Markdown" "blog_markdown.md" "https://mediatemple.net/blog/wp-content/uploads/2016/04/chris-coyier-4-960x406.png"
@@ -28,5 +59,3 @@ write_post "Jeffrey Uberstine" "_ju" "2020-04-10" "Linux Administration For Web 
 write_post "Jeffrey Uberstine" "_ju" "2020-04-10" "Linux Administration For Web Developers: Part 2" "linux_admin_web_dev2.md" "https://mediatemple.net/blog/wp-content/uploads/2020/04/1665x705-Part2-960x407.png"
 write_post "Alex Alabbas" "_aa" "2020-04-22" "Logs And Metrics: How Important Are They?" "logs_and_metrics.md" "https://mediatemple.net/blog/wp-content/uploads/2020/04/3330x1410-1-960x407.png"
 
-
-#sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' file
